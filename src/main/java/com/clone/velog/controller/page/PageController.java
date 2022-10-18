9877d30fb696934d750aaf2cf92c9df71db04088
DESKTOP-EVDUNQ5\LJW <ljwwnd99@gmail.com>
2022-10-18 12:54:29 +0900

    login session html 수정 완료

--- src/main/java/com/clone/velog/controller/page/PageController.java
+++ src/main/java/com/clone/velog/controller/page/PageController.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,13 +16,13 @@ import com.clone.velog.service.user.UserService;
 
 // PageController : UI에서 사용자가 요청하는 페이지를 띄움
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class PageController {
 
     @Autowired
     private UserService userService;
     
-    @RequestMapping("")
+    @RequestMapping("/home")
     public ModelAndView index(HttpServletRequest request, HttpServletResponse response, Model model){
         HttpSession session = request.getSession();
         if(session.getAttribute("userRes") != null){
@@ -54,7 +52,7 @@ public class PageController {
     public ModelAndView logout(HttpServletRequest request, HttpServletResponse response, Model model){
         HttpSession session = request.getSession();
         session.invalidate();; // session delete
-        model.addAttribute("loginURL", "/fragemet/header");
+        model.addAttribute("loginURL", "/fragment/header");
         return new ModelAndView("index")
             .addObject("code", "index");
     }
