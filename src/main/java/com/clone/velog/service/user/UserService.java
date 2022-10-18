823d6d3b38a9f544a7dc8093efd014194d038a55
DESKTOP-EVDUNQ5\LJW <ljwwnd99@gmail.com>
2022-10-18 11:31:55 +0900

    index url 추가, login session 수정 필요

--- src/main/java/com/clone/velog/service/user/UserService.java
+++ src/main/java/com/clone/velog/service/user/UserService.java
@@ -1,8 +1,94 @@
 package com.clone.velog.service.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.clone.velog.itf.CrudInterface;
+import com.clone.velog.models.entity.user.UserEntity;
+import com.clone.velog.models.network.Header;
+import com.clone.velog.models.network.response.UserRes;
+import com.clone.velog.repository.UserRepository;
+
 @Service
-public class UserService {
+public class UserService implements CrudInterface {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Header create(Header request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String read(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Header update(Header request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String delete(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Header<UserRes> login(String id) throws Exception{
+        Thread.sleep(300);
+        return userRepository.findByUserid(id)
+            .map(user -> response(user))
+            .orElseGet(
+                () -> Header.ERROR("데이터 없음")
+            );
+    }
     
+    private Header<UserRes> response(UserEntity user){
+        UserRes userRes = UserRes.builder()
+            .id(user.getId())
+            .userid(user.getUserid())
+            .userpw(user.getUserpw())
+            .name(user.getName())
+            .nickName(user.getNickName())
+            .hp(user.getHp())
+            .title(user.getTitle())
+            .descript(user.getDescript())
+            .email(user.getEmail())
+            .git(user.getGit())
+            .twitter(user.getTwitter())
+            .facebook(user.getFacebook())
+            .homePage(user.getHomePage())
+            .isAggreeEmail(user.getIsAggreeEmail())
+            .isAggreeUpdate(user.getIsAggreeUpdate())
+            .regdate(user.getRegdate())
+            .build();
+        return Header.OK(userRes);
+    }
+
+    private UserRes responseOnlyUser(UserEntity user){
+        UserRes userRes = UserRes.builder()
+            .id(user.getId())
+            .userid(user.getUserid())
+            .userpw(user.getUserpw())
+            .name(user.getName())
+            .nickName(user.getNickName())
+            .hp(user.getHp())
+            .title(user.getTitle())
+            .descript(user.getDescript())
+            .email(user.getEmail())
+            .git(user.getGit())
+            .twitter(user.getTwitter())
+            .facebook(user.getFacebook())
+            .homePage(user.getHomePage())
+            .isAggreeEmail(user.getIsAggreeEmail())
+            .isAggreeUpdate(user.getIsAggreeUpdate())
+            .regdate(user.getRegdate())
+            .build();
+        return userRes;
+    }
 }
