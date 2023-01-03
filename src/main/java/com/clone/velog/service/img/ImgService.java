1f6e7f37bc95c5c7181cb05dcfb7e6058d5d2af1
이중원 <ljwwnd99@gmail.com>
2023-01-03 15:07:35 +0900

    additional update

--- src/main/java/com/clone/velog/service/img/ImgService.java
+++ src/main/java/com/clone/velog/service/img/ImgService.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
